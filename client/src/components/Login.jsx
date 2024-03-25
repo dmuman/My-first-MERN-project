dfeeec0549c8394b5e641db6c4cdaee6b781d2c2
dmuman <umanskyi.dmytro@gmail.com>
2024-03-25 15:59:14 +0000

    axios -> Axios

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     Axios.defaults.withCredentials = true;
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.post('http://localhost:3001/auth/login', { email, password })
+        Axios.post('http://localhost:3001/auth/login', { email, password })
         .then(response => {
             if (response.data.status) {
                 navigate('/')
